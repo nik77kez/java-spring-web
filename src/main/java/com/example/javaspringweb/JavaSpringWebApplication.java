cc76925d118cf966e1363229b08fa2b4ff052b1b
Nikita Kezin <nik77kez@gmail.com>
2020-11-29 21:12:52 +0200

    Added basic files

---
+++ src/main/java/com/example/javaspringweb/JavaSpringWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.javaspringweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringWebApplication.class, args);
+	}
+
+}
